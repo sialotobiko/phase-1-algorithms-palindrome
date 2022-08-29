e15e609d61ef03396bbf15735ef2ec5e14e56d7f
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-29 23:55:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let palindrome = word.split('').reverse().join('');
+  let checkPalindrome = palindrome === word ? true : false
+
+  return checkPalindrome;
 }
 
 /* 
   Add your pseudocode here
+
+  function isPalindrome(word)
+  if word is equal to palindrome
+    return true
+   else
+    return false
+  end if  
+  
 */
 
 /*
   Add written explanation of your solution here
+
+  declare a function isPalindrome and pass it a string argument word.
+  declare a variable palindrome and assign it the value of the letters in reverse order
+  declare a variable checkPalindrome and assign it a condition testing whether palindome equals the original word
 */
 
 // You can run `node index.js` to view these console logs
